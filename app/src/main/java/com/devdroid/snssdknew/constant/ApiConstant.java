db56f95e4e7b137b0837124fd504bbc75e1f5c37
Gaolei <gurecn@gmail.com>
2017-07-23 10:49:03 +0800

    获取图片数量调整到20个

--- app/src/main/java/com/devdroid/snssdknew/constant/ApiConstant.java
+++ app/src/main/java/com/devdroid/snssdknew/constant/ApiConstant.java
@@ -11,5 +11,5 @@ public class ApiConstant {
     //下拉获取更新
     public static final String GET_SNSSDK_URL_FRESH = "http://ic.snssdk.com/neihan/stream/mix/v1/?mpic=1&content_type=-102&count=30&iid=5804114303&device_id=%s";
     //图片接口
-    public static final String GET_SNSSDK_IMAGE_URL_FRESH = "http://iu.snssdk.com/neihan/stream/category/data/v2/?mpic=1&webp=1&category_id=12&level=6&count=30&iid=11285516691&device_id=%s";
+    public static final String GET_SNSSDK_IMAGE_URL_FRESH = "http://iu.snssdk.com/neihan/stream/category/data/v2/?mpic=1&webp=1&category_id=12&level=6&count=20&iid=11285516691&device_id=%s";
 }
