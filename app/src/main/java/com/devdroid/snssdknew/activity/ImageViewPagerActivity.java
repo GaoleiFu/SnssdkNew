bc8ea46913b9c349564b2cc9d7188ac8d1dd363b
Gaolei <gurecn@gmail.com>
2017-10-25 12:47:54 +0800

    优化图片显示

--- app/src/main/java/com/devdroid/snssdknew/activity/ImageViewPagerActivity.java
+++ app/src/main/java/com/devdroid/snssdknew/activity/ImageViewPagerActivity.java
@@ -1,6 +1,7 @@
 package com.devdroid.snssdknew.activity;
 
 import android.content.Intent;
+import android.os.Build;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,13 +11,14 @@ import com.devdroid.snssdknew.adapter.ImagePagerAdapter;
 import com.devdroid.snssdknew.base.BaseActivity;
 
 public class ImageViewPagerActivity extends BaseActivity {
-
     private ViewPager mVpImage;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_view_pager);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            getWindow().addFlags(67108864);
+        }
         mVpImage = (ViewPager)findViewById(R.id.vp_image_view_pager);
         initData();
     }
--- app/src/main/java/com/devdroid/snssdknew/adapter/ImagePagerAdapter.java
+++ app/src/main/java/com/devdroid/snssdknew/adapter/ImagePagerAdapter.java
@@ -1,6 +1,5 @@
 package com.devdroid.snssdknew.adapter;
 
-import android.graphics.Bitmap;
 import android.support.v4.view.PagerAdapter;
 import android.view.View;
 import android.view.ViewGroup;
